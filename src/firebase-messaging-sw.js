c11a95d22ee4274907e9c7137591a35c0a6824df
mayur.saner <mayursaner05@gmail.com>
2020-08-11 19:39:30 +0530

    sw

--- src/firebase-messaging-sw.js
+++ src/firebase-messaging-sw.js
@@ -4,13 +4,16 @@ importScripts('https://www.gstatic.com/firebasejs/5.5.0/firebase-messaging.js');
 
 
 firebase.initializeApp({
-  'messagingSenderId': '417755979900'
+  messagingSenderId: '417755979900',
+  apiKey: "AIzaSyC_vNfvnni0876y0iHoC29IPUI8lf7WiLc",
+  projectId: "hrmsnotification-3a10f",
+  appId: "1:417755979900:web:a194861e7c546f007469f9",
 });
 
 const messaging = firebase.messaging();
 
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('../firebase-messaging-sw.js')
+  navigator.serviceWorker.register('firebase-messaging-sw.js')
     .then(function(registration) {
       console.log('Registration successful, scope is:', registration.scope);
     }).catch(function(err) {
