b4057e92d785c8d14c7accd1fee7021ea9482fc2
mayur.saner <mayursaner05@gmail.com>
2020-08-11 18:58:10 +0530

    firebase sw

--- src/firebase-messaging-sw.js
+++ src/firebase-messaging-sw.js
@@ -8,3 +8,12 @@ firebase.initializeApp({
 });
 
 const messaging = firebase.messaging();
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('../firebase-messaging-sw.js')
+    .then(function(registration) {
+      console.log('Registration successful, scope is:', registration.scope);
+    }).catch(function(err) {
+      console.log('Service worker registration failed, error:', err);
+    });
+  }
