5ed298931cf850159a9848164bccb4f9f93288de
Mayur Saner <mayur.saner@theblueflamelabs.com>
2020-08-12 16:53:25 +0530

    added cors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,55 @@
 
+// import { Component } from '@angular/core';
+// import { MessagingService } from "./shared/messaging.service";
+// import { MastersService } from 'src/shared/services/masters.service';
+
+
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css']
+// })
+// export class AppComponent {
+//   title = 'Notifications POC';
+//   message;
+
+
+//   constructor(private messagingService: MessagingService, private _masterservice: MastersService) { }
+
+//   ngOnInit() {
+
+//     //this.getProj();
+//     const userId = 'user001';
+//     this.messagingService.requestPermission(userId)
+//     this.messagingService.receiveMessage()
+//     this.message = this.messagingService.currentMessage
+
+//   }
+
+//   // getProj() {
+//   //   this._masterservice.getAllProjects().subscribe((result) => {
+//   //     console.log(result);
+//   //   });
+//   // }
+//  }
+
+
 import { Component } from '@angular/core';
-import { MessagingService } from "./shared/messaging.service";
 import { EmpServiceService } from './-emp-service.service';
 import { Employee } from './modules/modules.module';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Notification POC';
   empDetails: any = {};
   getAllEmps: any;
-  message;
-
-
-  constructor(private messagingService: MessagingService, private apiService: EmpServiceService) { }
-
+  title = 'herokudemo';
+  constructor(private apiService: EmpServiceService) { }
   ngOnInit() {
-    
-    const userId = 'user001';
-    this.messagingService.requestPermission(userId)
-    this.messagingService.receiveMessage()
-    this.message = this.messagingService.currentMessage
     this.getEmps();
   }
-
   getEmps() {
     this.apiService.getAllEmps().subscribe((data) => {
       this.getAllEmps = data;
@@ -39,4 +62,4 @@ export class AppComponent {
       });
     }
   }
- }
+}
